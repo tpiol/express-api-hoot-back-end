f8d5a0e4c4b50411aab149fa590742e9b1058d3d
Tia Piol <tiarox02@yahoo.com>
2025-08-19 10:07:20 -0700

    built the index route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -15,7 +15,15 @@ router.post("/", verifyToken, async (req, res) => {
     }
 });
 
-
+// GET /hoots
+router.get("/", verifyToken, async (req, res) => {
+    try {
+        const hoots = await Hoot.find({}).populate("author").sort({ createdAt: "desc"})
+        res.status(200).json(hoots);
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+})
 
 
 
