363166463f9ff60356a768c3c516fc1e920c7dd7
Tia Piol <tiarox02@yahoo.com>
2025-08-19 07:17:31 -0700

    init commit

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+username: {
+    type: String,
+    required: true
+},
+password: {
+    type: String, 
+    required: true,
+}
+});
+
+userSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        delete returnedObject.password;
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
