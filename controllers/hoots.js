644a113372156a281ac592e63558eb4363516009
Tia Piol <tiarox02@yahoo.com>
2025-08-19 10:14:56 -0700

    created show route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -18,13 +18,37 @@ router.post("/", verifyToken, async (req, res) => {
 // GET /hoots
 router.get("/", verifyToken, async (req, res) => {
     try {
-        const hoots = await Hoot.find({}).populate("author").sort({ createdAt: "desc"})
+        const hoots = await Hoot.find({}).populate("author").sort({ createdAt: "desc" })
         res.status(200).json(hoots);
     } catch (err) {
         res.status(500).json({ err: err.message })
     }
-})
+});
 
+// GET / hoots /: hootId
+router.get("/:hootId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate("author");
+        res.status(200).json(hoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+
+// PUT /hoots/:hootId
+router.put("/:hootId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId);
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+        const updatedHoot = await Hoot.findByIdAndUpdate
+
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
 
 
 module.exports = router;
